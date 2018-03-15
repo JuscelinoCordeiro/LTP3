a379ebb815b3a34c78ac0a929bdbca87c4cb7f43
hefesto <hefesto@portege>
2018-03-15 07:08:00 -0300

    criada a classe data em 15-03-18

---
+++ src/revisao/Data.java
@@ -0,0 +1,35 @@
+package revisao;
+
+public class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+//		if(this.mes in [1,3,5,7,8,10,12]){
+//			
+//		}
+		this.dia = dia;
+	}
+
+}
